1fe0c9d7d6f36ff17367f32a275986b5a7aa1f46
heysungj <linjing0525@gmail.com>
2022-09-11 21:48:35 -0400

    functional without css

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let equal = document.querySelector("#equal");
+let answerHtml = document.querySelector("h1");
+let form = document.querySelector("form");
+let answer = 0;
+let temp = "";
+
+form.addEventListener("click", (event) => {
+  event.preventDefault();
+  let value = event.target.id;
+  answerHtml.innerHTML = value;
+  temp = temp + value;
+  console.log(temp);
+});
+
+// equal to
+
+equal.addEventListener("click", () => {
+  answerHtml.innerHTML = eval(temp);
+  temp = "";
+});
